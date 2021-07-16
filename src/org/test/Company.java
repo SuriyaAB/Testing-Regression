3100e14a1f691da6f0d0633b01b8782bf582d7db
SuriyaAB <suriyabalasubramaniyan151@gmail.com>
2021-07-16 19:29:46 +0530

    Testing reg

---
+++ src/org/test/Company.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+public static void main(String[] args) {
+	List li =new ArrayList();
+	li.add(10);
+	li.add(20);
+	li.add(30);
+	li.add(40);
+	
+		System.out.println(li);
+}
+}
